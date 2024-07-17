919faaef395a910417bf57e0abd133a5d481b9cc
karthik v <karthikv@karthiks-MacBook-Pro.local>
2024-07-17 19:00:17 +0530

    updated the code

--- vars/helloKv.groovy
+++ vars/helloKv.groovy
@@ -1,6 +1,9 @@
 // vars/helloKv.groovy
+import kv.utils.Calculator
 
-def call(String name) {
+def call(String name, int first, int second) {
+    def result = Calculator.add(first, second)
+    echo "Result is ${result}"
     echo "Hello, ${name}!"
 }
 
