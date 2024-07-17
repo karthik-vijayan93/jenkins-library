5d38ae0b7d7c81679aa7d8f8fe5ba9f3a2b42bbb
karthik v <karthikv@karthiks-MacBook-Pro.local>
2024-07-17 18:53:14 +0530

    reverted teh changes

--- src/kv/utils/Calulator.groovy
+++ src/kv/utils/Calulator.groovy
@@ -1,8 +1,12 @@
 // src/kv/utils/Calculator.groovy
 package kv.utils
 
-class Calculator {
-    static int add(int first, int second) {
-        return first + second
-    }
+// class Calculator {
+//     static int add(int first, int second) {
+//         return first + second
+//     }
+// }
+
+def add(first, second){
+    return first + second
 }
--- vars/helloKv.groovy
+++ vars/helloKv.groovy
@@ -1,9 +1,6 @@
 // vars/helloKv.groovy
-import kv.utils.Calculator
 
 def call(String name) {
-    def result = Calculator.add(1, 2)
-    echo "Result is ${result}"
     echo "Hello, ${name}!"
 }
 
