85c3c00c980c80b693c960792812efe57aa0c753
karthik v <karthikv@karthiks-MacBook-Pro.local>
2024-07-17 18:50:45 +0530

    added fucntions

--- src/kv/utils/Calulator.groovy
+++ src/kv/utils/Calulator.groovy
@@ -1,7 +1,8 @@
+// src/kv/utils/Calculator.groovy
 package kv.utils
 
 class Calculator {
-    static int add(int first, int second){
+    static int add(int first, int second) {
         return first + second
     }
 }
--- vars/helloKv.groovy
+++ vars/helloKv.groovy
@@ -1,11 +1,11 @@
+// vars/helloKv.groovy
 import kv.utils.Calculator
 
 def call(String name) {
-    def result = Calculator.add(1,2)
+    def result = Calculator.add(1, 2)
     echo "Result is ${result}"
     echo "Hello, ${name}!"
 }
-
 
 def karthik(String kv) {
     echo "HI, ${kv}"
