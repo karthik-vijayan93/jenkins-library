bcc55b99c4844143235e4b29ff0132cb9aee3577
karthik v <karthikv@karthiks-mbp.in.ibm.com>
2024-07-18 15:48:53 +0530

    added credentials id support

--- vars/gitRef.groovy
+++ vars/gitRef.groovy
@@ -1,10 +1,11 @@
 #!/usr/bin/env groovy
 
-def call(String url, String ref, String subDir) {
+def call(String url, String ref, String subDir, String credentialsId = 'tonyaps') {
     dir(subDir) {
         deleteDir()
         checkout scm: [$class: 'GitSCM',
-                       userRemoteConfigs: [[url: url]],
+                       userRemoteConfigs: [[url: url,
+                                            credentialsId: credentialsId]],
                        extensions: [[$class: 'CloneOption',
                                      noTags: true,
                                      shallow: true,  
