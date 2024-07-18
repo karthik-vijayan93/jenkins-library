8e90385952771080e29c546f8353ffbbdaad702b
karthik v <karthikv@karthiks-mbp.in.ibm.com>
2024-07-18 15:30:38 +0530

    modified the script

--- resources/scripts/create_pr.py
+++ resources/scripts/create_pr.py
@@ -1,26 +1,33 @@
-
 import sys
 import requests
 
-def create_pull_request(repo_url, source_branch, destination_branch, pr_title):
-    api_url = repo_url.replace('https://github.com/', 'https://api.github.com/repos/') + '/pulls'
+def create_pull_request(repo_url: str, source_branch: str, destination_branch: str, pr_title: str, pr_description: str, token: str):
+    api_url = repo_url.replace('https://github.ibm.com/', 'https://api.github.ibm.com/repos/') + '/pulls'
     pr_data = {
         'title': pr_title,
         'head': source_branch,
-        'base': destination_branch
+        'base': destination_branch,
+        'body': pr_description
     }
-    response = requests.post(api_url, json=pr_data)
-    
-    if response.status_code == 201:
-        print("Pull request created successfully.")
-        print(response.json())
-    else:
-        print(f"Failed to create pull request: {response.status_code}")
-        print(response.json())
+    headers = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github+json'
+    }
+    try:
+        response = requests.post(api_url, json=pr_data, headers=headers)
+        if response.status_code == 201:
+            print("Pull request created successfully.")
+        else:
+            print(f"Failed to create pull request: {response.status_code}")
+            print(response.json())
+    except Exception as e:
+        print(e)
 
 if __name__ == "__main__":
     repo_url = sys.argv[1]
     source_branch = sys.argv[2]
     destination_branch = sys.argv[3]
     pr_title = sys.argv[4]
-    create_pull_request(repo_url, source_branch, destination_branch, pr_title)
+    pr_description = sys.argv[5]
+    token = sys.argv[6] 
+    create_pull_request(repo_url, source_branch, destination_branch, pr_title, pr_description, token)
--- vars/createPR.groovy
+++ vars/createPR.groovy
@@ -1,10 +1,12 @@
 
-def call(String repoUrl, String sourceBranch, String destinationBranch, String prTitle) {
+def call(String repoUrl, String sourceBranch, String destinationBranch, String prTitle, String prBody, String credentialsId ) {
     def scriptPath = libraryResource('scripts/create_pr.py')
     writeFile file: 'create_pr.py', text: scriptPath
 
-    sh """
-        pip3 install requests
-        python3 create_pr.py ${repoUrl} ${sourceBranch} ${destinationBranch} "${prTitle}"
-    """
+    withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'GITHUB_TOKEN', usernameVariable: 'GITHUB_USERNAME')]) {
+        sh """
+            pip3 install requests
+            python3 create_pr.py ${repoUrl} ${sourceBranch} ${destinationBranch} "${prTitle}" "${prBody}" ${GITHUB_TOKEN}
+        """
+    }
 }
